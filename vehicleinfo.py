d7e253549b70518f27c59d4e1c3f31e02d3e5a73
tuckerbrooks <32171110+tuckerbrooks@users.noreply.github.com>
2017-10-03 09:28:02 -0400

    List of models

--- vehicleinfo.py
+++ vehicleinfo.py
@@ -1,8 +1,6 @@
 make = [
 	"Audi"
-	"BMW"
 	"Chevrolet"
-	"Chrysler"
 	"Dodge"
 	"Ford"
 	"Honda"
@@ -16,3 +14,152 @@ make = [
 	"Volkswagen"
 ]
 
+audiModel = [
+	"A3"
+	"A4"
+	"A5"
+	"A6"
+	"A7"
+	"A8"
+	"Q3"
+	"Q5"
+	"TT"
+]
+
+chevyModel = [
+	"Avalanche"
+	"Aveo"
+	"Camaro"
+	"Colorado"
+	"Corvette"
+	"Cruze"
+	"Equinox"
+	"HHR"
+	"Impala"
+	"Malibu"
+	"Silverado"
+	"Tahoe"
+	"Traverse"
+]
+
+dodgeModel = [
+	"Avenger"
+	"Caliber"
+	"Challenger"
+	"Charger"
+	"Dakota"
+	"Durango"
+	"Grand Caravan"
+	"Journey"
+	"Nitro"
+	"Ram 1500"
+]
+
+fordModel = [
+	"Crown Victoria"
+	"Edge"
+	"Escape"
+	"Expedition"
+	"Explorer"
+	"F150"
+	"Fiesta"
+	"Flex"
+	"Focus"
+	"Fusion"
+	"Mustang"
+	"Ranger"
+	"Taurus"
+]
+
+hondaModel = [
+	"Accord"
+	"Civic"
+	"CR-V"
+	"Fit"
+	"Odyssey"
+	"Pilot"
+	"Ridgeline"
+]
+
+hyundaiModel = [
+	"Accent"
+	"Elantra"
+	"Santa Fe"
+	"Sonata"
+	"Tuscon"
+]
+
+jeepModel = [
+	"Compass"
+	"Grand Cherokee"
+	"Liberty"
+	"Patriot"
+	"Wrangler"
+]
+
+kiaModel = [
+	"Forte"
+	"Optima"
+	"Rio"
+	"Sorento"
+	"Soul"
+	"Sportage"
+]
+
+mazdaModel = [
+	"2"
+	"3"
+	"5"
+	"6"
+	"CX-5"
+	"CX-9"
+	"MX-5"
+]
+
+nissanModel = [
+	"Altima"
+	"Armada"
+	"Frontier"
+	"GT-R"
+	"Juke"
+	"Maxima"
+	"Murano"
+	"Pathfinder"
+	"Rogue"
+	"Sentra"
+	"Versa"
+]
+
+subaruModel = [
+	"BRZ"
+	"Crosstrek"
+	"Forester"
+	"Impreza"
+	"Legacy"
+	"Outback"
+	"WRX"
+]
+
+toyotaModel = [
+	"4Runner"
+	"Avalon"
+	"Camry"
+	"Corolla"
+	"Highlander"
+	"Prius"
+	"RAV4"
+	"Sequoia"
+	"Sienna"
+	"Tacoma"
+	"Tundra"
+	"Yaris"
+]
+
+vwModel = [
+	"Beetle"
+	"Golf"
+	"Jetta"
+	"Passat"
+	"Tiguan"
+	"Touareg"
+]
