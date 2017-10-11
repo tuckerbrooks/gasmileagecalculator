22f869eee51088e9e00c599930a8cb96429b2225
tuckerbrooks <32171110+tuckerbrooks@users.noreply.github.com>
2017-10-11 10:36:22 -0400

    Added vehicle models to run

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import csv
-import re
-import sys
 
 def main():
-	pass
+	getVehicleInfo()
+	deleteVehicles(getMake())
+	for vehicle in vehicles:
+		pass
+		#print (vehicle.make, vehicle.model)
 
 class Vehicle():
 	def __init__(self, citympg, combmpg, highwaympg, year, make, model, cylinders, displacement, transmission, drive):
@@ -20,11 +22,28 @@ class Vehicle():
 
 vehicles = []
 
-vehicleInfo = csv.DictReader(open('vehicles.csv'))
-for row in vehicleInfo:
-	vehicles.append(Vehicle(row["citympg"], row["combmpg"], row["highwaympg"], row["year"], row["make"], row["model"], row["cylinders"], row["displacement"], row["transmission"], row["drive"]))
+def getVehicleInfo():
+	vehicleInfo = csv.DictReader(open('vehicles.csv'))
+	for row in vehicleInfo:
+		vehicles.append(Vehicle(row["citympg"], row["combmpg"], row["highwaympg"], row["year"], row["make"], row["model"], row["cylinders"], row["displacement"], row["transmission"], row["drive"]))
 
-for vehicle in vehicles:
-	print(vehicle.citympg, vehicle.model)
+def getMake():
+	seen = set()
+	unique = []
+	sortedVehicles = sorted(vehicles, key=lambda vehicle: vehicle.make)
+	for vehicle in sortedVehicles:
+		if vehicle.make not in seen:
+			unique.append(vehicle.make)
+			seen.add(vehicle.make)
+	for make in unique:
+		print(make)
+	return input("Which make is your vehicle? (Make sure capitalization and spelling is correct on all questions)")
+
+def deleteVehicles(attribute):
+	out = []
+	for vehicle in vehicles:
+		if vehicle.make == attribute:
+			out.append(vehicle)
+			print(vehicle.make, vehicle.model)
 
 main()
